afeb68741bf6b5fcc40baf90d6aeef58f426ab63
Tbugaloo12 <90410801+Tbugaloo12@users.noreply.github.com>
2021-10-28 12:06:01 -0400

    Update v0.0 variable py
    Inserted the variables

---
+++ .github/variable/variables_practice.py
@@ -0,0 +1,11 @@
+#Variables Practice, Terrell Fuller, 10/28/21 11:41, version 0.0
+
+# Variable Types -- Most Common
+
+my_float = -2.43423
+my_int = -123456789
+my_string = "I like NBA"
+
+print(my_float)
+print(my_int)
+print(my_string)
